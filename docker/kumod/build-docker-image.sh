6b85cfb2eec51512ff80ce8398b51587a93f0cae
Wez Furlong <wez@wezfurlong.org>
2023-02-23 07:38:12 -0700

    CI: don't require a tty to build docker image

--- docker/kumod/build-docker-image.sh
+++ docker/kumod/build-docker-image.sh
@@ -2,7 +2,7 @@
 set -ex
 
 # compile a static binary using musl libc
-sudo docker run --rm -it -v "$(pwd)":/home/rust/src messense/rust-musl-cross:x86_64-musl cargo build --release
+sudo docker run --rm -v "$(pwd)":/home/rust/src messense/rust-musl-cross:x86_64-musl cargo build --release
 
 # Copy that binary into the docker build context
 cp target/x86_64-unknown-linux-musl/release/kumod docker/smtpd/
