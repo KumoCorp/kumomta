af653a489f30104416c7c0731bb2159ed537f5d9
Wez Furlong <wez@wezfurlong.org>
2023-02-23 10:16:48 -0700

    make docker build work with older versions of docker

--- docker/kumod/build-docker-image.sh
+++ docker/kumod/build-docker-image.sh
@@ -2,4 +2,4 @@
 set -ex
 top_dir="$(git rev-parse --show-toplevel)"
 cd "${top_dir}"
-docker build -t kumomta/kumod --build-context "src=${top_dir}" ./docker/kumod
+docker build -t kumomta/kumod --file docker/kumod/Dockerfile .
