602e9721c98a74b997af7044b09f2d96c977fc1e
Wez Furlong <wez@wezfurlong.org>
2023-02-23 07:57:08 -0700

    CI: fixup path for rename from smtpd -> kumod

--- docker/kumod/build-docker-image.sh
+++ docker/kumod/build-docker-image.sh
@@ -5,8 +5,8 @@ set -ex
 sudo docker run --rm -v "$(pwd)":/home/rust/src messense/rust-musl-cross:x86_64-musl cargo build --release
 
 # Copy that binary into the docker build context
-cp target/x86_64-unknown-linux-musl/release/kumod docker/smtpd/
-trap "rm docker/smtpd/kumod" EXIT
+cp target/x86_64-unknown-linux-musl/release/kumod docker/kumod/
+trap "rm docker/kumod/kumod" EXIT
 
 # Build image
-sudo docker build -t kumomta/smtpd ./docker/smtpd
+sudo docker build -t kumomta/kumod ./docker/kumod
