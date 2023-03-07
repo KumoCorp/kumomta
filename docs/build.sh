3e97cea1f626fc47a6af6cdbf861b252fa49f02d
Wez Furlong <wez@wezfurlong.org>
2023-03-07 11:31:01 -0700

    docs build: simplify dep logic a little

--- docs/build.sh
+++ docs/build.sh
@@ -4,9 +4,11 @@ tracked_markdown=$(mktemp)
 trap "rm ${tracked_markdown}" "EXIT"
 git ls-tree -r HEAD --name-only docs | egrep '\.(markdown|md)$' > $tracked_markdown
 
-if ! hash gelatyx 2>/dev/null ; then
-  cargo install gelatyx --locked
-fi
+for doc_dep in gelatyx mdbook mdbook-linkcheck mdbook-mermaid mdbook.admonish ; do
+  if ! hash $doc_dep 2>/dev/null ; then
+    cargo install $doc_dep --locked
+  fi
+done
 
 gelatyx --language lua --file-list $tracked_markdown --language-config stylua.toml
 gelatyx --language lua --file-list $tracked_markdown --language-config stylua.toml --check || exit 1
@@ -14,19 +16,6 @@ gelatyx --language lua --file-list $tracked_markdown --language-config stylua.to
 set -x
 
 python3 docs/generate-toc.py || exit 1
-
-if ! hash mdbook 2>/dev/null ; then
-  cargo install mdbook --locked
-fi
-if ! hash mdbook-linkcheck 2>/dev/null ; then
-  cargo install mdbook-linkcheck --locked
-fi
-if ! hash mdbook-mermaid 2>/dev/null ; then
-  cargo install mdbook-mermaid --locked
-fi
-if ! hash mdbook-admonish 2>/dev/null ; then
-  cargo install mdbook-admonish --locked
-fi
 
 mdbook-mermaid install docs
 mdbook build docs
