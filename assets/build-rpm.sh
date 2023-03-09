18c0b10fc1724c684d698148fffa9738977d6a25
Wez Furlong <wez@wezfurlong.org>
2023-03-08 21:31:06 -0700

    add script that can build and rpm from an already built repo

---
+++ assets/build-rpm.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+set -x
+set -e
+TAG_NAME=${TAG_NAME:-$(git -c "core.abbrev=8" show -s "--format=%cd-%h" "--date=format:%Y.%m.%d")}
+HERE=$(pwd)
+
+KUMO_RPM_VERSION=$(echo ${TAG_NAME} | tr - _)
+distroid=$(sh -c "source /etc/os-release && echo \$ID" | tr - _)
+distver=$(sh -c "source /etc/os-release && echo \$VERSION_ID" | tr - _)
+export SOURCE_DATE_EPOCH=$(git log -1 --pretty=%ct)
+
+spec=$(mktemp)
+trap "rm ${spec}" "EXIT"
+
+cat > $spec <<EOF
+Name: kumomta
+Version: ${KUMO_RPM_VERSION}
+Release: 1.${distroid}${distver}
+Packager: Wez Furlong <wez@wezfurlong.org>
+License: MIT
+URL: https://kumomta.com
+Summary: A high performance, modern MTA.
+
+%description
+A high performance, modern MTA.
+
+%build
+echo "Doing the build bit here"
+
+%changelog
+See product changelog
+
+%install
+set -x
+cd ${HERE}
+mkdir -p %{buildroot}/usr/bin 
+install -Dsm755 target/release/kumod -t %{buildroot}/usr/bin
+install -Dsm755 target/release/traffic-gen -t %{buildroot}/usr/bin
+
+%files
+/usr/bin/kumod
+/usr/bin/traffic-gen
+EOF
+
+/usr/bin/rpmbuild -bb $spec --verbose
