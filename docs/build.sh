bf99848ef05ed508a9ff54dfdf5e7994f5193aac
Wez Furlong <wez@wezfurlong.org>
2023-03-11 06:11:58 -0700

    ci: surface unformatted lua code errors pushed to main
    the pages workflow silently corrects these so they go unnoticed,
    leaving the corrections to show up for the next person to run
    `docs/build.sh`, polluting their later commit with unrelated
    changes.
    
    Augment the verify-pages workflow to also run on `main` and
    surface the corrections with a failed workflow run.

--- docs/build.sh
+++ docs/build.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
+set -e
 
 tracked_markdown=$(mktemp)
 trap "rm ${tracked_markdown}" "EXIT"
 git ls-tree -r HEAD --name-only docs | egrep '\.(markdown|md)$' > $tracked_markdown
 
-for doc_dep in gelatyx mdbook mdbook-linkcheck mdbook-mermaid mdbook.admonish ; do
+for doc_dep in gelatyx mdbook mdbook-linkcheck mdbook-mermaid mdbook-admonish ; do
   if ! hash $doc_dep 2>/dev/null ; then
     cargo install $doc_dep --locked
   fi
 done
 
-gelatyx --language lua --file-list $tracked_markdown --language-config stylua.toml
-gelatyx --language lua --file-list $tracked_markdown --language-config stylua.toml --check || exit 1
+if test -z "${CHECK_ONLY}" ; then
+  gelatyx --language lua --file-list $tracked_markdown --language-config stylua.toml 
+fi
+if ! gelatyx --language lua --file-list $tracked_markdown --language-config stylua.toml --check ; then
+  echo
+  echo "Be sure to run ./docs/build.sh to apply formatting before you push changes."
+  echo
+  exit 1
+fi
 
 set -x
 
