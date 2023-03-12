8a1bf0b7a3abd7f056e406995678c5540c28fbb8
Wez Furlong <wez@wezfurlong.org>
2023-03-11 20:52:21 -0700

    docs: cleanup toc structure a bit
    Remove a redundant parent node to simplify things.
    A couple of Page entries were just containers and
    had no content of their own; you can now set the filename
    to None to indicate that, and adjust what gets emitted
    in the nav.

--- docs/generate-toc.py
+++ docs/generate-toc.py
@@ -27,7 +27,10 @@ class Page(object):
                     output.write(f'{indent}{bullet}"{self.title}": {self.filename}\n')
                 else:
                     output.write(f'{indent}{bullet}"{self.title}":\n')
-                    output.write(f'{indent}  {bullet}"{self.title}": {self.filename}\n')
+                    if self.filename:
+                        output.write(
+                            f'{indent}  {bullet}"{self.title}": {self.filename}\n'
+                        )
         for kid in self.children:
             kid.render(output, depth + 1, mode)
 
@@ -75,101 +78,95 @@ class Gen(object):
 
 TOC = [
     Page(
-        "KumoMTA Documentation",
-        "index.md",
+        "General Information",
+        None,
         children=[
-            Page(
-                "General Information",
-                "general/index.md",
-                children=[
-                    Page("Preface and Legal Notices", "preface/index.md"),
-                    Page("About This Manual", "general/about.md"),
-                    Page("How to Report Bugs or Get Help", "general/report.md"),
-                    Page("Credits", "general/credits.md"),
-                    Page("History", "overview/history.md"),
-                    Page("Architecture", "overview/architecture.md"),
-                ],
-            ),
-            Page(
-                "User Guide",
-                "guide/index.md",
-                children=[
-                    Page("Getting Started", "guide/getting_started.md"),
-                    Page(
-                        "Environmental Considerations",
-                        "guide/subs/environment_consideration.md",
-                    ),
-                    Page("System Preparation", "guide/subs/system_preparation.md"),
-                    Page(
-                        "Installing for Development",
-                        "guide/subs/install_for_development.md",
-                    ),
-                    Page(
-                        "Installing for Production",
-                        "guide/subs/install_for_production_use.md",
-                    ),
-                    Page(
-                        "Special instructions for CentOS7 users",
-                        "guide/subs/special_for_centos7.md",
-                    ),
-                    Page("Your First Email", "guide/subs/your_first_email.md"),
-                    Page(
-                        "Beyond Basics",
-                        "guide/beyond_basics.md",
-                        children=[
-                            Page("Configuring DKIM", "guide/subs/dkim.md"),
-                            Page("Configuring TLS", "guide/subs/tls.md"),
-                        ],
-                    ),
-                    Page(
-                        "Advanced Configurations",
-                        "guide/advanced_config.md",
-                        children=[
-                            Page("Lua Resources", "guide/subs/lua_resources.md"),
-                            Page("Lua Functions", "guide/subs/lua_functions.md"),
-                        ],
-                    ),
-                ],
-            ),
-            Page(
-                "Reference Manual",
-                "reference/index.md",
-                children=[
-                    Page("Queues", "reference/queues.md"),
-                    Gen(
-                        "module: kumo",
-                        "reference/kumo",
-                    ),
-                    Gen(
-                        "module: kumo.dkim",
-                        "reference/kumo.dkim",
-                    ),
-                    Gen(
-                        "module: sqlite",
-                        "reference/sqlite",
-                    ),
-                    Gen(
-                        "module: redis",
-                        "reference/redis",
-                    ),
-                    Gen(
-                        "object: address",
-                        "reference/address",
-                    ),
-                    Gen(
-                        "object: message",
-                        "reference/message",
-                    ),
-                    Gen(
-                        "events",
-                        "reference/events",
-                    ),
-                    Gen("HTTP API", "reference/http", extract_title=True),
-                ],
-            ),
-            Page("Change Log", "changelog.md"),
+            Page("Preface and Legal Notices", "preface/index.md"),
+            Page("About This Manual", "general/about.md"),
+            Page("How to Report Bugs or Get Help", "general/report.md"),
+            Page("Credits", "general/credits.md"),
+            Page("History", "overview/history.md"),
+            Page("Architecture", "overview/architecture.md"),
         ],
-    )
+    ),
+    Page(
+        "User Guide",
+        None,
+        children=[
+            Page("Getting Started", "guide/getting_started.md"),
+            Page(
+                "Environmental Considerations",
+                "guide/subs/environment_consideration.md",
+            ),
+            Page("System Preparation", "guide/subs/system_preparation.md"),
+            Page(
+                "Installing for Development",
+                "guide/subs/install_for_development.md",
+            ),
+            Page(
+                "Installing for Production",
+                "guide/subs/install_for_production_use.md",
+            ),
+            Page(
+                "Special instructions for CentOS7 users",
+                "guide/subs/special_for_centos7.md",
+            ),
+            Page("Your First Email", "guide/subs/your_first_email.md"),
+            Page(
+                "Beyond Basics",
+                "guide/beyond_basics.md",
+                children=[
+                    Page("Configuring DKIM", "guide/subs/dkim.md"),
+                    Page("Configuring TLS", "guide/subs/tls.md"),
+                ],
+            ),
+            Page(
+                "Advanced Configurations",
+                "guide/advanced_config.md",
+                children=[
+                    Page("Lua Resources", "guide/subs/lua_resources.md"),
+                    Page("Lua Functions", "guide/subs/lua_functions.md"),
+                ],
+            ),
+        ],
+    ),
+    Page(
+        "Reference Manual",
+        "reference/index.md",
+        children=[
+            Page("Queues", "reference/queues.md"),
+            Gen(
+                "module: kumo",
+                "reference/kumo",
+            ),
+            Gen(
+                "module: kumo.dkim",
+                "reference/kumo.dkim",
+            ),
+            Gen(
+                "module: sqlite",
+                "reference/sqlite",
+            ),
+            Gen(
+                "module: redis",
+                "reference/redis",
+            ),
+            Gen(
+                "object: address",
+                "reference/address",
+            ),
+            Gen(
+                "object: message",
+                "reference/message",
+            ),
+            Gen(
+                "events",
+                "reference/events",
+            ),
+            Gen("HTTP API", "reference/http", extract_title=True),
+        ],
+    ),
+    Page("Change Log", "changelog.md"),
 ]
 
 mode = sys.argv[1]
@@ -181,7 +178,7 @@ if mode == "mkdocs":
         f.write("INHERIT: mkdocs-base.yml\n")
         f.write("nav:\n")
         for page in TOC:
-            page.render(f, mode="mkdocs")
+            page.render(f, depth=1, mode="mkdocs")
 
 elif mode == "mdbook":
     with open("SUMMARY.md", "w") as f:
