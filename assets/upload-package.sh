b4f9280757b63220bb76b7b191e72d701e0a921b
Wez Furlong <wez@wezfurlong.org>
2023-03-13 13:17:19 -0700

    upload-packages: remove stray --help

--- assets/upload-package.sh
+++ assets/upload-package.sh
@@ -15,7 +15,7 @@ trap "rm -f $t" EXIT
 printenv TOKEN > $t
 
 for pkg in $PACKAGES ; do
-  curl --help -X POST https://openrepo.kumomta.com/api/$REPO/upload/ \
+  curl -X POST https://openrepo.kumomta.com/api/$REPO/upload/ \
       -H "Authorization: Token $(< $t)" \
       -F "package_file=@$pkg" -i
 done
