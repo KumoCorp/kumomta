c2cb5b798b6d336072d6f51202baf929a66eff22
Wez Furlong <wez@wezfurlong.org>
2023-03-14 14:14:37 -0700

    fix centos7 build, adjust docker to use /opt/kumomta
    Revert building static/bundled sqlite, as the logic deep in sqlite3-src
    makes no attempt to deal with this mess on centos7:
    
      = note: /opt/rh/devtoolset-9/root/usr/libexec/gcc/x86_64-redhat-linux/9/ld: /__w/kumomta/kumomta/target/release/deps/libsqlite3_src-b3c44e6f34aac065.rlib(sqlite3.o):(.data.rel.aSyscall+0xb0): undefined reference to `fcntl64'
              collect2: error: ld returned 1 exit status
    
    (I don't blame it!)
    
    Deal with that in the docker image by simply installing sqlite
    in the docker image.
    
    And since I'm touching the docker stuff, adjust its install
    path and layout to match that of the rpm and deb packages.

--- docker/kumod/docker-runner.sh
+++ docker/kumod/docker-runner.sh
@@ -3,6 +3,6 @@ set -xe
 
 KUMO_POLICY="${KUMO_POLICY:-/config/policy.lua}"
 
-exec /app/kumod --policy "${KUMO_POLICY}" --user kumod
+exec /opt/kumomta/sbin/kumod --policy "${KUMO_POLICY}" --user kumod
 
 
