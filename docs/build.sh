0cf6a16ef27d5729fb44d0e5a0dffb63c9930127
Wez Furlong <wez@wezfurlong.org>
2023-03-23 08:28:17 -0700

    allow running `./docs/build.sh serve` to build and serve docs
    Any arguments you pass to it will be passed through to mkdocs

--- docs/build.sh
+++ docs/build.sh
@@ -2,19 +2,19 @@
 set -e
 set -x
 
+SERVE=no
+if [ "$1" == "serve" ] ; then
+  SERVE=yes
+fi
+
 tracked_markdown=$(mktemp)
 trap "rm ${tracked_markdown}" "EXIT"
 find docs -type f | egrep '\.(markdown|md)$' > $tracked_markdown
-
-mode=mkdocs
 
 # We use mdbook-linkcheck even with mkdocs because we could not find any
 # good mkdocs link checking tools that work properly with local links
 cargo_deps="gelatyx mdbook-linkcheck"
 
-if [ $mode == "mdbook" ]; then
-  cargo_deps="$cargo_deps mdbook mdbook-mermaid mdbook-admonish"
-fi
 for doc_dep in $cargo_deps ; do
   if ! hash $doc_dep 2>/dev/null ; then
     cargo install $doc_dep --locked
@@ -31,26 +31,22 @@ if ! gelatyx --language lua --color always --file-list $tracked_markdown --langu
   exit 1
 fi
 
-python3 docs/generate-toc.py $mode || exit 1
+python3 docs/generate-toc.py || exit 1
 
-case $mode in
-  mkdocs)
-    # Run mdbook-linkcheck and clean up its junk
-    mdbook-linkcheck --standalone docs
+mdbook-linkcheck --standalone docs
 
-    # Adjust path to pick up pip-installed binaries
-    PATH="$HOME/.local/bin;$PATH"
-    pip install --quiet mkdocs-material mkdocs-git-revision-date-localized-plugin black mkdocs-exclude
-    if test -n "${CARDS}" ; then
-      pip install --quiet pillow cairosvg
-    fi
-    mkdocs build
-    # Keep the toc generator formatted
-    black docs/generate-toc.py
-    ;;
-  mdbook)
-    mdbook-mermaid install docs
-    mdbook build docs
-    ;;
-esac
+# Adjust path to pick up pip-installed binaries
+PATH="$HOME/.local/bin;$PATH"
+pip install --quiet mkdocs-material mkdocs-git-revision-date-localized-plugin black mkdocs-exclude
+if test -n "${CARDS}" ; then
+  pip install --quiet pillow cairosvg
+fi
 
+# Keep the toc generator formatted
+black docs/generate-toc.py
+
+if [ "$SERVE" == "yes" ] ; then
+  mkdocs "$@"
+else
+  mkdocs build
+fi
