KumoCorp/kumomta

commit 0842a0fc8b5fc4499f977c2d908628aaf1e21192

ready_queue: disambiguate queue names by protocol
Include `@protocol-info` on the end of the ready queue names.

This prevents surprising false sharing of custom delivery protocols
when returning different protocols based on the tenant or campaign
metadata.

This is visible in the stats:

```json
  "total_connection_count": {
    "help": "total number of active connections ever made",
    "type": "counter",
    "value": {
      "service": {
        "smtp_client": 1.0,
        "smtp_client:source2->(in1-smtp|in2-smtp).messagingengine.com@smtp": 1.0
      }
    }
  },
```

and perhaps unexpectedly in the `site_name` field of the json log
records. That will need to get tidied up in a separate commit.

crates/kumod/src/queue.rs
@@ -39,6 +39,16 @@ pub enum DeliveryProto {
     Lua { custom_lua: LuaDeliveryProtocol },
 }
 
+impl DeliveryProto {
+    pub fn ready_queue_name(&self) -> String {
+        match self {
+            Self::Smtp => "smtp".to_string(),
+            Self::Maildir { maildir_path } => format!("maildir:{}", maildir_path.display()),
+            Self::Lua { custom_lua } => format!("lua:{}", custom_lua.constructor),
+        }
+    }
+}
+
 impl Default for DeliveryProto {
     fn default() -> Self {
         Self::Smtp

crates/kumod/src/ready_queue.rs
@@ -89,11 +89,24 @@ impl ReadyQueueManager {
             None
         };
 
+        // Note well! The ready queue name is based on the perspective of the
+        // receiver, combining our source (which they see) with the unique
+        // name of the destination (which we compute from the MX site name).
+        // For custom protocols where we don't have an MX record and thus no
+        // site name, we simply use the domain; we do not include the campaign
+        // or tenant because those have no bearing from the perspective of
+        // the recipient.
         let site_name = mx
             .as_ref()
             .map(|mx| mx.site_name.to_string())
             .unwrap_or_else(|| components.domain.to_string());
-        let name = format!("{egress_source}->{site_name}");
+        // Factor in the delivery protocol so that we don't falsely share
+        // different custom protocols when someone is using eg: just the
+        // tenant or campaign to vary the protocol.
+        let name = format!(
+            "{egress_source}->{site_name}@{}",
+            queue_config.protocol.ready_queue_name()
+        );
 
         let mut config = load_config().await?;
 

commit bcd2946c53a868e1e780dd8eb0a0898c0902b18e

make_queue_config: now supports protocol.smtp.mx_list for smart hosting
Previously, you would do either:
  `msg:set_meta('queue', 'smart.host.domain')`
or
  `msg:set_meta('queue', '[10.0.0.1]')`

to override the effective domain for a message and cause it to be routed
to somewhere other than the recipient domain.

That was OK for basic smart hosting, but limiting when you wanted to use
multiple candidate hosts.

This commit expands the queue config `protocol` field to support
specifying an explicit list of MX hosts that should be used instead.

The integration tests have been migrated away from the old style to this
new style.

While adding plumbing for this, I uncovered an inconsistency between the
queue name generated for the ready queue and the name used by suspension
handling. The inconsistency was introduced in
0842a0fc8b5fc4499f977c2d908628aaf1e21192 and related work.  This commit
resolves it.

crates/dns-resolver/src/lib.rs
@@ -34,6 +34,65 @@ fn fully_qualify(domain_name: &str) -> ResolveResult<Name> {
     Ok(name)
 }
 
+pub async fn resolve_a_or_aaaa(domain_name: &str) -> anyhow::Result<Vec<ResolvedAddress>> {
+    if domain_name.starts_with('[') {
+        // It's a literal address, no DNS lookup necessary
+
+        if !domain_name.ends_with(']') {
+            anyhow::bail!(
+                "domain_name `{domain_name}` is a malformed literal \
+                     domain with no trailing `]`"
+            );
+        }
+
+        let lowered = domain_name.to_ascii_lowercase();
+        let literal = &lowered[1..lowered.len() - 1];
+
+        if let Some(v6_literal) = literal.strip_prefix("ipv6:") {
+            match v6_literal.parse::<Ipv6Addr>() {
+                Ok(addr) => {
+                    return Ok(vec![ResolvedAddress {
+                        name: domain_name.to_string(),
+                        addr: std::net::IpAddr::V6(addr),
+                    }]);
+                }
+                Err(err) => {
+                    anyhow::bail!("invalid ipv6 address: `{v6_literal}`: {err:#}");
+                }
+            }
+        }
+
+        // Try to interpret the literal as either an IPv4 or IPv6 address.
+        // Note that RFC5321 doesn't actually permit using an untagged
+        // IPv6 address, so this is non-conforming behavior.
+        match literal.parse::<IpAddr>() {
+            Ok(addr) => {
+                return Ok(vec![ResolvedAddress {
+                    name: domain_name.to_string(),
+                    addr,
+                }]);
+            }
+            Err(err) => {
+                anyhow::bail!("invalid address: `{literal}`: {err:#}");
+            }
+        }
+    }
+
+    match ip_lookup(domain_name).await {
+        Ok((addrs, _expires)) => {
+            let addrs = addrs
+                .iter()
+                .map(|&addr| ResolvedAddress {
+                    name: domain_name.to_string(),
+                    addr,
+                })
+                .collect();
+            Ok(addrs)
+        }
+        Err(err) => anyhow::bail!("{err:#}"),
+    }
+}
+
 impl MailExchanger {
     pub async fn resolve(domain_name: &str) -> anyhow::Result<Arc<Self>> {
         if domain_name.starts_with('[') {

crates/kumod/src/egress_source.rs
@@ -1,6 +1,6 @@
 use crate::http_server::admin_suspend_ready_q_v1::AdminSuspendReadyQEntry;
 use crate::queue::QueueConfig;
-use crate::ready_queue::ReadyQueueManager;
+use crate::ready_queue::{ReadyQueueManager, ReadyQueueName};
 use anyhow::Context;
 use config::LuaConfig;
 use gcd::Gcd;
@@ -370,7 +370,9 @@ impl EgressPoolRoundRobin {
 
         // filter to non-suspended pathways
         for entry in &self.entries {
-            if let Ok(path_name) =
+            if let Ok(ReadyQueueName {
+                name: path_name, ..
+            }) =
                 ReadyQueueManager::compute_queue_name(queue_name, queue_config, &entry.name).await
             {
                 match AdminSuspendReadyQEntry::get_for_queue_name(&path_name) {

crates/kumod/src/queue.rs
@@ -4,6 +4,7 @@ use crate::http_server::admin_suspend_v1::AdminSuspendEntry;
 use crate::logging::{log_disposition, LogDisposition, RecordType};
 use crate::lua_deliver::LuaDeliveryProtocol;
 use crate::ready_queue::ReadyQueueManager;
+use crate::smtp_dispatcher::SmtpProtocol;
 use crate::spool::SpoolManager;
 use anyhow::Context;
 use chrono::Utc;
@@ -34,7 +35,7 @@ lazy_static::lazy_static! {
 #[derive(Deserialize, Serialize, Debug, Clone)]
 #[serde(untagged)]
 pub enum DeliveryProto {
-    Smtp,
+    Smtp { smtp: SmtpProtocol },
     Maildir { maildir_path: std::path::PathBuf },
     Lua { custom_lua: LuaDeliveryProtocol },
 }
@@ -42,7 +43,7 @@ pub enum DeliveryProto {
 impl DeliveryProto {
     pub fn metrics_protocol_name(&self) -> &'static str {
         match self {
-            Self::Smtp => "smtp_client",
+            Self::Smtp { .. } => "smtp_client",
             Self::Maildir { .. } => "maildir",
             Self::Lua { .. } => "lua",
         }
@@ -51,7 +52,7 @@ impl DeliveryProto {
     pub fn ready_queue_name(&self) -> String {
         let proto_name = self.metrics_protocol_name();
         match self {
-            Self::Smtp => proto_name.to_string(),
+            Self::Smtp { .. } => proto_name.to_string(),
             Self::Maildir { maildir_path } => format!("{proto_name}:{}", maildir_path.display()),
             Self::Lua { custom_lua } => format!("{proto_name}:{}", custom_lua.constructor),
         }
@@ -60,7 +61,9 @@ impl DeliveryProto {
 
 impl Default for DeliveryProto {
     fn default() -> Self {
-        Self::Smtp
+        Self::Smtp {
+            smtp: SmtpProtocol::default(),
+        }
     }
 }
 
@@ -562,7 +565,7 @@ impl Queue {
     async fn insert_ready(&mut self, msg: Message) -> anyhow::Result<()> {
         tracing::trace!("insert_ready {}", msg.id());
         match &self.queue_config.protocol {
-            DeliveryProto::Smtp | DeliveryProto::Lua { .. } => {
+            DeliveryProto::Smtp { .. } | DeliveryProto::Lua { .. } => {
                 // rr_attempts is a bit gross; ideally rr.next would know how
                 // to inspect the egress_path.suspended configuration and reflect
                 // that in the RoundRobinResult, but it doesn't have a reference

crates/kumod/src/ready_queue.rs
@@ -33,6 +33,20 @@ lazy_static::lazy_static! {
     static ref MANAGER: Mutex<ReadyQueueManager> = Mutex::new(ReadyQueueManager::new());
 }
 
+pub struct ReadyQueueName {
+    pub name: String,
+    pub site_name: String,
+    pub mx: Option<Arc<MailExchanger>>,
+}
+
+pub struct ReadyQueueConfig {
+    pub name: String,
+    pub site_name: String,
+    pub path_config: EgressPathConfig,
+    pub egress_source: EgressSource,
+    pub mx: Option<Arc<MailExchanger>>,
+}
+
 #[derive(Default)]
 pub struct ReadyQueueManager {
     queues: HashMap<String, ReadyQueueHandle>,
@@ -55,44 +69,9 @@ impl ReadyQueueManager {
         queue_name: &str,
         queue_config: &QueueConfig,
         egress_source: &str,
-    ) -> anyhow::Result<String> {
+    ) -> anyhow::Result<ReadyQueueName> {
         let components = QueueNameComponents::parse(queue_name);
-
-        let needs_mx = matches!(&queue_config.protocol, DeliveryProto::Smtp);
-
-        let mx = if needs_mx {
-            Some(MailExchanger::resolve(components.domain).await?)
-        } else {
-            None
-        };
-
-        let site_name = mx
-            .as_ref()
-            .map(|mx| mx.site_name.to_string())
-            .unwrap_or_else(|| components.domain.to_string());
-        let name = format!("{egress_source}->{site_name}");
-        Ok(name)
-    }
-
-    async fn compute_config(
-        queue_name: &str,
-        queue_config: &QueueConfig,
-        egress_source: &str,
-    ) -> anyhow::Result<(
-        String,
-        EgressPathConfig,
-        EgressSource,
-        Option<Arc<MailExchanger>>,
-    )> {
-        let components = QueueNameComponents::parse(queue_name);
-
-        let needs_mx = matches!(&queue_config.protocol, DeliveryProto::Smtp);
-
-        let mx = if needs_mx {
-            Some(MailExchanger::resolve(components.domain).await?)
-        } else {
-            None
-        };
+        let mut mx = None;
 
         // Note well! The ready queue name is based on the perspective of the
         // receiver, combining our source (which they see) with the unique
@@ -101,10 +80,18 @@ impl ReadyQueueManager {
         // site name, we simply use the domain; we do not include the campaign
         // or tenant because those have no bearing from the perspective of
         // the recipient.
-        let site_name = mx
-            .as_ref()
-            .map(|mx| mx.site_name.to_string())
-            .unwrap_or_else(|| components.domain.to_string());
+        let site_name = match &queue_config.protocol {
+            DeliveryProto::Smtp { smtp } => {
+                if smtp.mx_list.is_empty() {
+                    mx.replace(MailExchanger::resolve(components.domain).await?);
+                    mx.as_ref().unwrap().site_name.to_string()
+                } else {
+                    format!("mx_list:{}", smtp.mx_list.join(","))
+                }
+            }
+            _ => components.domain.to_string(),
+        };
+
         // Factor in the delivery protocol so that we don't falsely share
         // different custom protocols when someone is using eg: just the
         // tenant or campaign to vary the protocol.
@@ -113,6 +100,26 @@ impl ReadyQueueManager {
             queue_config.protocol.ready_queue_name()
         );
 
+        Ok(ReadyQueueName {
+            name,
+            site_name,
+            mx,
+        })
+    }
+
+    async fn compute_config(
+        queue_name: &str,
+        queue_config: &QueueConfig,
+        egress_source: &str,
+    ) -> anyhow::Result<ReadyQueueConfig> {
+        let ReadyQueueName {
+            name,
+            site_name,
+            mx,
+        } = Self::compute_queue_name(queue_name, queue_config, egress_source).await?;
+
+        let components = QueueNameComponents::parse(queue_name);
+
         let mut config = load_config().await?;
 
         let egress_source = EgressSource::resolve(egress_source, &mut config).await?;
@@ -120,7 +127,11 @@ impl ReadyQueueManager {
         let path_config: EgressPathConfig = config
             .async_call_callback(
                 "get_egress_path_config",
-                (components.domain, egress_source.name.to_string(), site_name),
+                (
+                    components.domain,
+                    egress_source.name.to_string(),
+                    site_name.clone(),
+                ),
             )
             .await
             .map_err(|err| {
@@ -128,7 +139,13 @@ impl ReadyQueueManager {
                 err
             })?;
 
-        Ok((name, path_config, egress_source, mx))
+        Ok(ReadyQueueConfig {
+            name,
+            site_name,
+            path_config,
+            egress_source,
+            mx,
+        })
     }
 
     pub async fn resolve_by_queue_name(
@@ -137,8 +154,13 @@ impl ReadyQueueManager {
         egress_source: &str,
         egress_pool: &str,
     ) -> anyhow::Result<ReadyQueueHandle> {
-        let (name, path_config, egress_source, mx) =
-            Self::compute_config(queue_name, queue_config, egress_source).await?;
+        let ReadyQueueConfig {
+            name,
+            site_name: _,
+            path_config,
+            egress_source,
+            mx,
+        } = Self::compute_config(queue_name, queue_config, egress_source).await?;
 
         if path_config.suspended {
             return Err(ReadyQueueSuspended.into());
@@ -208,7 +230,7 @@ impl ReadyQueueManager {
                         )
                         .await
                         {
-                            Ok((_name, path_config, _egress_source, _mx)) => {
+                            Ok(ReadyQueueConfig { path_config, .. }) => {
                                 // TODO: ideally we'd impl PartialEq for path_config
                                 // and its recursive types, but that is currently
                                 // non-trivial, so we do a gross debug repr comparison
@@ -531,7 +553,7 @@ impl Dispatcher {
         let activity = Activity::get(format!("ready_queue Dispatcher {name}"))?;
 
         let delivery_protocol = match &queue_config.protocol {
-            DeliveryProto::Smtp => "ESMTP".to_string(),
+            DeliveryProto::Smtp { .. } => "ESMTP".to_string(),
             DeliveryProto::Lua { .. } => "Lua".to_string(),
             DeliveryProto::Maildir { .. } => "Maildir".to_string(),
         };
@@ -555,10 +577,12 @@ impl Dispatcher {
         };
 
         let mut queue_dispatcher: Box<dyn QueueDispatcher> = match &queue_config.protocol {
-            DeliveryProto::Smtp => match SmtpDispatcher::init(&mut dispatcher).await? {
-                Some(disp) => Box::new(disp),
-                None => return Ok(()),
-            },
+            DeliveryProto::Smtp { smtp } => {
+                match SmtpDispatcher::init(&mut dispatcher, smtp).await? {
+                    Some(disp) => Box::new(disp),
+                    None => return Ok(()),
+                }
+            }
             DeliveryProto::Lua {
                 custom_lua: proto_config,
             } => {

crates/kumod/src/smtp_dispatcher.rs
@@ -4,14 +4,21 @@ use crate::ready_queue::{Dispatcher, QueueDispatcher};
 use crate::spool::SpoolManager;
 use anyhow::Context;
 use async_trait::async_trait;
-use dns_resolver::ResolvedMxAddresses;
+use dns_resolver::{resolve_a_or_aaaa, ResolvedMxAddresses};
 use kumo_api_types::egress_path::Tls;
 use kumo_log_types::ResolvedAddress;
 use kumo_server_lifecycle::ShutdownSubcription;
 use kumo_server_runtime::{rt_spawn, spawn};
 use message::Message;
 use rfc5321::{ClientError, EnhancedStatusCode, ForwardPath, Response, ReversePath, SmtpClient};
+use serde::{Deserialize, Serialize};
 use std::net::SocketAddr;
+
+#[derive(Deserialize, Serialize, Debug, Clone, Default)]
+pub struct SmtpProtocol {
+    #[serde(default)]
+    pub mx_list: Vec<String>,
+}
 
 #[derive(Debug)]
 pub struct SmtpDispatcher {
@@ -22,7 +29,10 @@ pub struct SmtpDispatcher {
 }
 
 impl SmtpDispatcher {
-    pub async fn init(dispatcher: &mut Dispatcher) -> anyhow::Result<Option<Self>> {
+    pub async fn init(
+        dispatcher: &mut Dispatcher,
+        proto_config: &SmtpProtocol,
+    ) -> anyhow::Result<Option<Self>> {
         let path_config = dispatcher.path_config.borrow().clone();
         let ehlo_name = match &path_config.ehlo_domain {
             Some(n) => n.to_string(),
@@ -35,12 +45,20 @@ impl SmtpDispatcher {
             },
         };
 
-        let addresses = dispatcher
-            .mx
-            .as_ref()
-            .expect("to have mx when doing smtp")
-            .resolve_addresses()
-            .await;
+        let addresses = if proto_config.mx_list.is_empty() {
+            dispatcher
+                .mx
+                .as_ref()
+                .expect("to have mx when doing smtp")
+                .resolve_addresses()
+                .await
+        } else {
+            let mut addresses = vec![];
+            for a in proto_config.mx_list.iter() {
+                addresses.append(&mut resolve_a_or_aaaa(a).await?);
+            }
+            ResolvedMxAddresses::Addresses(addresses)
+        };
         tracing::trace!("mx resolved to {addresses:?}");
 
         let mut addresses = match addresses {

crates/integration-tests/source.lua
@@ -74,10 +74,7 @@ if WEBHOOK_PORT then
   end)
 end
 
-kumo.on('smtp_server_message_received', function(msg)
-  -- Redirect traffic to the sink
-  msg:set_meta('queue', 'localhost')
-end)
+kumo.on('smtp_server_message_received', function(msg) end)
 
 kumo.on('get_queue_config', function(domain, _tenant, _campaign)
   if domain == 'webhook' then
@@ -89,7 +86,14 @@ kumo.on('get_queue_config', function(domain, _tenant, _campaign)
       },
     }
   end
-  return kumo.make_queue_config {}
+  return kumo.make_queue_config {
+    protocol = {
+      -- Redirect traffic to the sink
+      smtp = {
+        mx_list = { 'localhost' },
+      },
+    },
+  }
 end)
 
 kumo.on('get_egress_path_config', function(_domain, _source_name, _site_name)

crates/integration-tests/src/main.rs
@@ -64,7 +64,7 @@ DeliverySummary {
             .kcli_json([
                 "suspend-ready-q",
                 "--name",
-                "unspecified->localhost",
+                "unspecified->mx_list:localhost@smtp_client",
                 "--reason",
                 "testing",
             ])
@@ -122,7 +122,7 @@ DeliverySummary {
             .kcli_json([
                 "suspend-ready-q",
                 "--name",
-                "unspecified->localhost",
+                "unspecified->mx_list:localhost@smtp_client",
                 "--reason",
                 "testing",
             ])
@@ -204,7 +204,7 @@ DeliverySummary {
         let mut client = daemon.smtp_client().await?;
 
         let status: SuspendV1Response = daemon
-            .kcli_json(["suspend", "--domain", "localhost", "--reason", "testing"])
+            .kcli_json(["suspend", "--domain", "example.com", "--reason", "testing"])
             .await?;
         println!("kcli status: {status:?}");
 
@@ -255,7 +255,7 @@ DeliverySummary {
         let mut client = daemon.smtp_client().await?;
 
         let status: SuspendV1Response = daemon
-            .kcli_json(["suspend", "--domain", "localhost", "--reason", "testing"])
+            .kcli_json(["suspend", "--domain", "example.com", "--reason", "testing"])
             .await?;
         println!("kcli status: {status:?}");
 