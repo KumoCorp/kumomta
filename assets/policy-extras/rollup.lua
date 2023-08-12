7aa80bc34a6d9cdc380d98cb0d3d874c3ff6e6c0
Wez Furlong <wez@wezfurlong.org>
2023-08-12 06:34:07 -0700

    rollup.lua: add ip-based rollup
    How it works:
    
    * When the lowest preference MX host names match a pattern like
      `.mail.protection.outlook.com`, the message has its routing_domain
      set to a placeholder domain whose name ends with `.ip_rollup`.
    * That results in a scheduled queue name like
      `foo.com!outlook.ip_rollup`, which makes it possible to know both the
      original domain and the fact that rollup is in use.
    * `get_queue_config` can check to see if the routing_domain is set to
      something that ends with `.ip_rollup` to override the `mx_list`
      in the queue configuration with just the lowest preference IP
      addresses from the original domain.
    * Now, instead of computing a site_name base around
      `foo-com.mail.protection.outlook.com`, which includes the individual
      original recipient domain, and would cause there to be a separate
      ready queue for each domain, the overridden mx_list causes
      the site_name to be eg: `mx_list:[104.47.24.36],[104.47.25.36]`.
      That same site_name will be used for every domain that shares those
      same IP addresses
    * When `get_egress_path_config` is called to get shaping parameters,
      it is passed the routing domain `outlook.ip_rollup`. You can use that
      name with mx_rollup=false as the key in your shaping.toml, of if you
      are directly implementing `get_egress_path_config`, you can use that
      name to determine the appropriate configuration.
    
    ```lua
    kumo.on('get_queue_config', function(domain, tenant, campaign, routing_domain)
      local params = {}
      rollup.apply_ip_rollup_to_queue_config(domain, routing_domain, params
      return kumo.make_queue_config(params)
    end)
    
    kumo.on('smtp_server_message_received', function(msg)
      rollup.reroute_using_ip_rollup(msg, {
        ['.mail.protection.outlook.com.'] = 'outlook.ip_rollup',
      })
    end)
    ```
    
    In your shaping.toml:
    
    ```toml
    ["outlook.ip_rollup"]
    mx_rollup = false
     # shaping parameters here
    ```
    
    Caveats:
    
    * With this technique, we'll never try to use any of the lower
      priority/higher preference value MX records for any of the matching
      domains.
    * The IP addresses to which the MX host names resolve can vary over time.
      We'll still queue the mail to the same scheduled queue (eg:
      `foo.com!outlook.ip_rollup`), but it's possible for there to be
      multiple ready queues with different names based on those changed
      IPs.  This is actually a feature: if the destination domain has
      an outage and are now publishing different IPs, we'll pick those up
      and use them.
    * Since the ready queue names look like `mx_list:[104.47.24.36],[104.47.25.36]`
      it can be hard to intuit just from glancing at that name where those queues go.

--- assets/policy-extras/rollup.lua
+++ assets/policy-extras/rollup.lua
@@ -2,13 +2,141 @@ local mod = {}
 local kumo = require 'kumo'
 local utils = require 'policy-extras.policy_utils'
 
-local function all_hosts_match_suffix(mx, suffix)
-  for _, host in ipairs(mx.hosts) do
+local function all_hosts_match_suffix(hosts, suffix)
+  for _, host in ipairs(hosts) do
     if not utils.ends_with(host, suffix) then
       return false
     end
   end
   return true
+end
+
+local function compute_ip_rollup_mx_list(domain, routing_domain)
+  if routing_domain == nil then
+    return nil
+  end
+  if not utils.ends_with(routing_domain, '.ip_rollup') then
+    return nil
+  end
+
+  local mx = kumo.dns.lookup_mx(domain)
+
+  local lowest_pref = nil
+  local hosts = nil
+
+  for pref, pref_hosts in pairs(mx.by_pref) do
+    pref = tonumber(pref)
+    if lowest_pref == nil or pref < lowest_pref then
+      lowest_pref = pref
+      hosts = pref_hosts
+    end
+  end
+
+  local addrs = {}
+  -- The host names are pre-sorted by lookup_mx
+  for _, host in ipairs(hosts) do
+    local host_addrs = kumo.dns.lookup_addr(host)
+    -- The addresses are in an unspecified order.
+    -- Sort them so that we produce a consistent list
+    -- and hash consistently with the resulting ready queue name
+    table.sort(host_addrs)
+    for _, a in ipairs(host_addrs) do
+      table.insert(addrs, string.format('[%s]', a))
+    end
+  end
+
+  return addrs
+end
+
+function mod.apply_ip_rollup_to_queue_config(domain, routing_domain, params)
+  local mx_list = compute_ip_rollup_mx_list(domain, routing_domain)
+  if mx_list then
+    params.protocol = {
+      smtp = {
+        mx_list = mx_list,
+      },
+    }
+    print('Applied rollup', kumo.json_encode(params))
+  end
+end
+
+local function compute_ip_rollup(domain, mapping)
+  local mx = kumo.dns.lookup_mx(domain)
+
+  local lowest_pref = nil
+  local hosts = nil
+
+  -- Extract the lowest pref value (highest priority) set
+  -- of mx hosts from the mx records
+  for pref, pref_hosts in pairs(mx.by_pref) do
+    pref = tonumber(pref)
+    if lowest_pref == nil or pref < lowest_pref then
+      lowest_pref = pref
+      hosts = pref_hosts
+    end
+  end
+
+  -- Find a matching mapping entry
+  local routing_domain = nil
+  for suffix, routing_domain in pairs(mapping) do
+    if all_hosts_match_suffix(hosts, suffix) then
+      return routing_domain
+    end
+  end
+  return nil
+end
+
+--[[
+This function performs ip-based rollup for domains whose
+MX records match certain suffixies.
+
+The mapping parameter must be a map of mx hostname suffix
+to a synthetic routing domain name, which must also end
+with `.ip_rollup`.
+
+The recipient domain is resolved for the supplied message,
+and its lowest preferenced (eg: highest priority) mx records
+are compared against the hostname suffix.  If they all match,
+then the message has its routing domain set to the routing
+domain.
+
+In order to use this successfully, you must also arrange
+for your get_queue_config event to update the queue parameters:
+
+```lua
+kumo.on('get_queue_config', function(domain, tenant, campaign, routing_domain)
+  local params = {}
+  rollup.apply_ip_rollup_to_queue_config(domain, routing_domain, params
+  return kumo.make_queue_config(params)
+end)
+
+kumo.on('smtp_server_message_received', function(msg)
+  rollup.reroute_using_ip_rollup(msg, {
+    ['.mail.protection.outlook.com.'] = 'outlook.ip_rollup',
+  })
+end)
+```
+
+To apply shaping to the result, if you are using the shaping helper,
+you will need to use the ip rollup routing domain:
+
+```toml
+["outlook.ip_rollup"]
+mx_rollup = false
+# shaping parameters here
+```
+
+]]
+function mod.reroute_using_ip_rollup(msg, mapping)
+  local status, err = pcall(function()
+    local recip_domain = msg:recipient().domain
+    local routing_domain = compute_ip_rollup(recip_domain, mapping)
+    msg:set_meta('routing_domain', routing_domain)
+  end)
+  if not status then
+    -- Log, but otherwise ignore failure to resolve the domain
+    print(string.format('while checking ip_rollup: %s', err))
+  end
 end
 
 --[[
@@ -40,7 +168,7 @@ function mod.reroute_based_on_mx_host_suffix(msg, mapping)
     local recip_domain = msg:recipient().domain
     local mx = kumo.dns.lookup_mx(recip_domain)
     for suffix, routing_domain in pairs(mapping) do
-      if all_hosts_match_suffix(mx, suffix) then
+      if all_hosts_match_suffix(mx.hosts, suffix) then
         if recip_domain ~= routing_domain then
           msg:set_meta('routing_domain', routing_domain)
         end
