8b7776e34d68a4cc7fe37470cc3cb83b6aa72506
Wez Furlong <wez@wezfurlong.org>
2023-08-30 16:13:01 -0700

    mta-sts: parser/loader for policy dns and http info
    refs: https://github.com/KumoCorp/kumomta/issues/7

---
+++ crates/mta-sts/src/dns.rs
@@ -0,0 +1,117 @@
+use futures::future::BoxFuture;
+use std::collections::BTreeMap;
+use trust_dns_resolver::TokioAsyncResolver;
+
+// <https://datatracker.ietf.org/doc/html/rfc8461>
+
+#[derive(Debug)]
+pub struct MtaStsDnsRecord {
+    pub id: String,
+    pub fields: BTreeMap<String, String>,
+}
+
+/// A trait for entities that perform DNS resolution.
+pub trait Lookup: Sync + Send {
+    fn lookup_txt<'a>(&'a self, name: &'a str) -> BoxFuture<'a, anyhow::Result<Vec<String>>>;
+}
+
+impl Lookup for TokioAsyncResolver {
+    fn lookup_txt<'a>(&'a self, name: &'a str) -> BoxFuture<'a, anyhow::Result<Vec<String>>> {
+        Box::pin(async move {
+            self.txt_lookup(name)
+                .await?
+                .into_iter()
+                .map(|txt| {
+                    Ok(txt
+                        .iter()
+                        .map(|data| String::from_utf8_lossy(data))
+                        .collect())
+                })
+                .collect()
+        })
+    }
+}
+
+pub async fn resolve_dns_record(
+    policy_domain: &str,
+    resolver: &dyn Lookup,
+) -> anyhow::Result<MtaStsDnsRecord> {
+    let dns_name = format!("_mta-sts.{policy_domain}");
+    let res = resolver.lookup_txt(&dns_name).await?;
+    let txt = res.join("");
+
+    let mut fields = BTreeMap::new();
+
+    for pair in txt.split(';') {
+        if pair.is_empty() {
+            continue;
+        }
+        let (key, value) = pair.split_once('=').ok_or_else(|| {
+            anyhow::anyhow!("invalid element in STS text record: {pair}. Full record: {txt}")
+        })?;
+
+        let key = key.trim();
+        let value = value.trim();
+
+        fields.insert(key.to_string(), value.to_string());
+    }
+
+    if fields.get("v").map(|s| s.as_str()) != Some("STSv1") {
+        anyhow::bail!("TXT record is not an STSv1 record {txt}");
+    }
+
+    let id = fields
+        .get("id")
+        .ok_or_else(|| anyhow::anyhow!("STSv1 TXT record is missing id parameter. {txt}"))?
+        .to_string();
+
+    Ok(MtaStsDnsRecord { id, fields })
+}
+
+#[cfg(test)]
+pub(crate) mod test {
+    use super::*;
+
+    pub struct TestResolver {
+        dns: BTreeMap<&'static str, &'static str>,
+    }
+
+    impl TestResolver {
+        pub fn new<I: IntoIterator<Item = (&'static str, &'static str)>>(iter: I) -> Self {
+            Self {
+                dns: BTreeMap::from_iter(iter),
+            }
+        }
+    }
+
+    impl Lookup for TestResolver {
+        fn lookup_txt<'a>(&'a self, name: &'a str) -> BoxFuture<'a, anyhow::Result<Vec<String>>> {
+            Box::pin(async move {
+                match self.dns.get(name) {
+                    Some(result) => Ok(vec![result.to_string()]),
+                    None => anyhow::bail!("NXDOMAIN {name}"),
+                }
+            })
+        }
+    }
+
+    #[tokio::test]
+    async fn test_parse_dns_record() {
+        let resolver = TestResolver::new([("_mta-sts.gmail.com", "v=STSv1; id=20190429T010101;")]);
+
+        let result = resolve_dns_record("gmail.com", &resolver).await.unwrap();
+
+        k9::snapshot!(
+            result,
+            r#"
+MtaStsDnsRecord {
+    id: "20190429T010101",
+    fields: {
+        "id": "20190429T010101",
+        "v": "STSv1",
+    },
+}
+"#
+        );
+    }
+}
---
+++ crates/mta-sts/src/policy.rs
@@ -0,0 +1,168 @@
+use futures::future::BoxFuture;
+use std::collections::BTreeMap;
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum PolicyMode {
+    Enforce,
+    Testing,
+    None,
+}
+
+#[derive(Debug)]
+pub struct MtaStsPolicy {
+    pub mode: PolicyMode,
+    pub mx: Vec<String>,
+    pub max_age: usize,
+    pub fields: BTreeMap<String, Vec<String>>,
+}
+
+impl MtaStsPolicy {
+    pub fn parse(data: &str) -> anyhow::Result<Self> {
+        let mut fields: BTreeMap<String, Vec<String>> = BTreeMap::new();
+
+        for line in data.lines() {
+            if line.is_empty() {
+                continue;
+            }
+            let (key, value) = line
+                .split_once(':')
+                .ok_or_else(|| anyhow::anyhow!("invalid STS policy {data}"))?;
+            let key = key.trim();
+            let value = value.trim();
+
+            fields
+                .entry(key.to_string())
+                .or_default()
+                .push(value.to_string());
+        }
+
+        let version = fields
+            .remove("version")
+            .ok_or_else(|| anyhow::anyhow!("STS policy {data} is missing a version"))?;
+        if version.len() != 1 || version[0] != "STSv1" {
+            anyhow::bail!("STS policy {data} has incompatible STS version");
+        }
+
+        let mode = match fields.remove("mode") {
+            None => anyhow::bail!("STS policy {data} is missing required mode"),
+            Some(mode) if mode.len() == 1 => match mode[0].as_str() {
+                "enforce" => PolicyMode::Enforce,
+                "testing" => PolicyMode::Testing,
+                "none" => PolicyMode::None,
+                _ => anyhow::bail!("STS policy {data} has invalid mode"),
+            },
+            _ => anyhow::bail!("STS policy {data} has invalid mode"),
+        };
+
+        let mx = match fields.remove("mx") {
+            None if mode == PolicyMode::None => vec![],
+            None => anyhow::bail!("STS policy {data} is missing required mx"),
+            Some(v) => v,
+        };
+
+        let max_age: usize = match fields.remove("max_age") {
+            None => anyhow::bail!("STS policy {data} is missing required max_age"),
+            Some(v) if v.len() == 1 => {
+                let max_age = &v[0];
+                max_age.parse().map_err(|err| anyhow::anyhow!("STS policy {data} has max_age {max_age} that is not a valid integer: {err:#}"))?
+            }
+            _ => anyhow::bail!("STS policy {data} has invalid max_age"),
+        };
+
+        Ok(Self {
+            fields,
+            mode,
+            mx,
+            max_age,
+        })
+    }
+}
+
+pub trait Get: Sync + Send {
+    fn http_get<'a>(&'a self, url: &'a str) -> BoxFuture<'a, anyhow::Result<String>>;
+}
+
+pub async fn load_policy_for_domain(
+    policy_domain: &str,
+    getter: &dyn Get,
+) -> anyhow::Result<MtaStsPolicy> {
+    let url = format!("https://mta-sts.{policy_domain}/.well-known/mta-sts.txt");
+    let policy = getter.http_get(&url).await?;
+    MtaStsPolicy::parse(&policy)
+}
+
+#[cfg(test)]
+pub(crate) mod test {
+    use super::*;
+
+    pub struct TestGetter {
+        policies: BTreeMap<&'static str, &'static str>,
+    }
+
+    impl TestGetter {
+        pub fn new<I: IntoIterator<Item = (&'static str, &'static str)>>(iter: I) -> Self {
+            Self {
+                policies: BTreeMap::from_iter(iter),
+            }
+        }
+    }
+
+    impl Get for TestGetter {
+        fn http_get<'a>(&'a self, url: &'a str) -> BoxFuture<'a, anyhow::Result<String>> {
+            Box::pin(async move {
+                match self.policies.get(url) {
+                    Some(result) => Ok(result.to_string()),
+                    None => anyhow::bail!("404 {url}"),
+                }
+            })
+        }
+    }
+
+    const SAMPLE_POLICY: &str =
+        "version: STSv1 \nmode: enforce\nmx: mail.example.com\r\nmx:\t*.example.net\nmx: backupmx.example.com\nmax_age: 604800";
+
+    #[tokio::test]
+    async fn get_policy() {
+        let getter = TestGetter::new([(
+            "https://mta-sts.example.com/.well-known/mta-sts.txt",
+            SAMPLE_POLICY,
+        )]);
+
+        k9::snapshot!(
+            load_policy_for_domain("example.com", &getter)
+                .await
+                .unwrap(),
+            r#"
+MtaStsPolicy {
+    mode: Enforce,
+    mx: [
+        "mail.example.com",
+        "*.example.net",
+        "backupmx.example.com",
+    ],
+    max_age: 604800,
+    fields: {},
+}
+"#
+        );
+    }
+
+    #[test]
+    fn parse_policy() {
+        k9::snapshot!(
+            MtaStsPolicy::parse(SAMPLE_POLICY).unwrap(),
+            r#"
+MtaStsPolicy {
+    mode: Enforce,
+    mx: [
+        "mail.example.com",
+        "*.example.net",
+        "backupmx.example.com",
+    ],
+    max_age: 604800,
+    fields: {},
+}
+"#
+        );
+    }
+}
