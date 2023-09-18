5f616bfb15aa003c1e096e9c8d9fee0a71f964e5
Wez Furlong <wez@wezfurlong.org>
2023-09-17 19:10:28 -0700

    ci: retire these scripts
    These were for working around constraints in GH actions and since
    we aren't using it anymore for the main portion of our CI/CD,
    we don't need them anymore.

--- assets/trim-target-dir.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# This script only exists because GH Actions is pretty tight on disk space
-# It's purpose is to reduce the target dir down to just the build artifacts
-
-rm -rf ./target/{release,debug}/{example,build,deps}
-
