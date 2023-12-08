7ebebb83bba19c9fb75ec435e5e7266ce5d380f9
Wez Furlong <wez@wezfurlong.org>
2023-12-08 11:57:21 -0700

    ci: make signer fail loudly if it fails

--- assets/sign-rpm.sh
+++ assets/sign-rpm.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # Requires; PUB and PRIV env set to GPG public and private keys, populated
 # with from the GH secrets for the org/repo
+set -e
+
 PACKAGES="$@"
 
 t=$(mktemp)
@@ -15,6 +17,6 @@ gpg --batch --import $t
 # https://stackoverflow.com/a/57953409/149111
 # Without this, signing prompts for input even though there is no password,
 # which fails in the CI
-echo ""  | setsid rpmsign --define '_signature gpg' --define '_gpg_name KumoMTA Signing Key' --addsign $PACKAGES
+echo "" | setsid rpmsign --define '_signature gpg' --define '_gpg_name KumoMTA Signing Key' --addsign $PACKAGES
 
 echo "Signed $PACKAGES OK"
