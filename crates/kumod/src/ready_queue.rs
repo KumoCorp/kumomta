KumoCorp/kumomta

commit 3d127026059c3223ffb4c680f0cfe57e9004c382

amortize ready-q maintainer triggers
Previously, every insertion into the ready queue would cause the
maintainer logic to run in the context of the submitting thread.

For busy, high-concurrency systems this could result in multiple
dispatches of the maintainer for the same queue in quick succession.

This commit adjusts the triggering logic so that the existing
background maintainer task will be woken up to perform those
actions.

This reduces the number of redundant calls to the maintainer,
without harming latency.

crates/kumod/src/ready_queue.rs
@@ -196,16 +196,17 @@ impl ReadyQueueManager {
         let activity = Activity::get(format!("ReadyQueueHandle {name}"))?;
 
         let handle = manager.queues.entry(name.clone()).or_insert_with(|| {
+            let notify = Arc::new(Notify::new());
             rt_spawn_non_blocking(format!("maintain {name}"), {
                 let name = name.clone();
-                move || Ok(async move { Self::maintainer_task(name).await })
+                let notify = notify.clone();
+                move || Ok(async move { Self::maintainer_task(name, notify).await })
             })
             .expect("failed to spawn maintainer");
             let proto = queue_config.borrow().protocol.metrics_protocol_name();
             let service = format!("{proto}:{name}");
             let metrics = DeliveryMetrics::new(&service, &proto);
             let ready = Arc::new(StdMutex::new(VecDeque::new()));
-            let notify = Arc::new(Notify::new());
             ReadyQueueHandle(Arc::new(Mutex::new(ReadyQueue {
                 name: name.clone(),
                 queue_name_for_config_change_purposes_only: queue_name.to_string(),
@@ -226,7 +227,7 @@ impl ReadyQueueManager {
         Ok(handle.clone())
     }
 
-    async fn maintainer_task(name: String) -> anyhow::Result<()> {
+    async fn maintainer_task(name: String, notify: Arc<Notify>) -> anyhow::Result<()> {
         let mut shutdown = ShutdownSubcription::get();
         let mut interval = Duration::from_secs(60);
         let mut memory = subscribe_to_memory_status_changes();
@@ -240,6 +241,7 @@ impl ReadyQueueManager {
                     interval = Duration::from_secs(1);
                 },
                 _ = memory.changed() => {},
+                _ = notify.notified() => {},
             };
             let mut mgr = Self::get().await;
             let queue = { mgr.queues.get(&name).cloned() };
@@ -345,9 +347,8 @@ impl ReadyQueue {
             self.metrics.ready_count.inc();
         }
 
-        self.notify.notify_waiters();
-        self.maintain().await;
         self.last_change = Instant::now();
+        self.notify.notify_waiters();
 
         Ok(())
     }

commit 1df99483972f013242f23e052fa8796fdaaeac5e

fixup low traffic case for maintainer dispatch
I broke this in 3d127026059c3223ffb4c680f0cfe57e9004c382.
The fix is simple; we just need to trigger the maintain
method when the maintainer starts up.

crates/kumod/src/ready_queue.rs
@@ -231,6 +231,20 @@ impl ReadyQueueManager {
         let mut shutdown = ShutdownSubcription::get();
         let mut interval = Duration::from_secs(60);
         let mut memory = subscribe_to_memory_status_changes();
+
+        // Trigger an initial maintainer run; this case is hit when
+        // traffic is low and we get eg: just a single message
+        // enqueued. In that case we don't notice the wakup on
+        // `notify` in a timely manner and we'll sit waiting
+        // for a subsequent message.
+        {
+            let mgr = Self::get().await;
+            if let Some(queue) = mgr.queues.get(&name).cloned() {
+                let mut queue = queue.lock().await;
+                queue.maintain().await;
+            }
+        }
+
         loop {
             let mut refresh_config = false;
             tokio::select! {