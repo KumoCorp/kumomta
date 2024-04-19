1d17a3a5db2e429bfe9e994ddacb963840ad74ab
Wez Furlong <wez@wezfurlong.org>
2024-04-19 10:30:40 -0700

    kumod: logging: migrate from deprecated minijinja functions

--- crates/kumod/src/logging.rs
+++ crates/kumod/src/logging.rs
@@ -808,7 +808,7 @@ impl LogHookState {
 
         let mut record_text = Vec::new();
         self.template_engine
-            .add_global("log_record", minijinja::Value::from_serializable(&record));
+            .add_global("log_record", minijinja::Value::from_serialize(&record));
 
         if let Some(template) =
             Self::resolve_template(&self.params, &self.template_engine, record.kind)
@@ -1063,7 +1063,7 @@ impl LogThreadState {
         if let Some(file) = self.file_map.get_mut(&file_key) {
             let mut record_text = Vec::new();
             self.template_engine
-                .add_global("log_record", minijinja::Value::from_serializable(&record));
+                .add_global("log_record", minijinja::Value::from_serialize(&record));
 
             if let Some(template) =
                 Self::resolve_template(&self.params, &self.template_engine, record.kind)
