4ad21257bdc6fefec6dd27b89b070fc03701f51e
Wez Furlong <wez@wezfurlong.org>
2024-06-19 08:34:22 -0700

    ci: fixup failure in PR mode
    We need to avoid recursively copying any symlinks-to-dirs as
    directories. We can do that with cp, but the parallel xcp utility
    doesn't support the --no-dereference flag at this time.
    
    refs: https://github.com/tarka/xcp/issues/52

--- assets/ci/populate-cache-vars.sh
+++ assets/ci/populate-cache-vars.sh
@@ -6,15 +6,34 @@ PATH=$PATH:$HOME/.cargo/bin
 export CARGO_TARGET_DIR="${MAIN_TARGET}"
 export CARGO_HOME="/build-cache/cargo-home"
 
+# Workaround lack of --no-dereference in xcp.
+# The criteria is really "if source dir has symlinks-to-dirs",
+# but we compromise and approximate as "if source dir has symlinks to other than .so"
+# as a lower effort test to write here
+# <https://github.com/tarka/xcp/issues/52>
+function can_use_xcp() {
+  if hash xcp 2>/dev/null ; then
+    if find "$1" -type l | grep -q -v \.so ; then
+      # echo "Have symlinks"
+      return 1
+    fi
+    # echo "no symlinks"
+    return 0
+  else
+    # echo "no xcp"
+    return 0
+  fi
+}
+
 case "${CI_PIPELINE_EVENT}" in
   pull_request*)
     # Copy the mainline cache and use that as a basis
     CARGO_TARGET_DIR="${CI_WORKSPACE}/target"
     if test -d "${MAIN_TARGET}" ; then
-      if hash xcp 2>/dev/null ; then
+      if can_use_xcp "${MAIN_TARGET}" ; then
         time xcp --recursive --no-progress --workers=0 "${MAIN_TARGET}" "${CARGO_TARGET_DIR}"
       else
-        time cp -p --recursive "${MAIN_TARGET}" "${CARGO_TARGET_DIR}"
+        time cp -p --recursive --no-dereference "${MAIN_TARGET}" "${CARGO_TARGET_DIR}"
       fi
     fi
   ;;
