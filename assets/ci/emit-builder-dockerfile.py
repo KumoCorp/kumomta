7ad26cdac93c126731318faa188c820876ed72ae
Wez Furlong <wez@wezfurlong.org>
2024-07-04 12:31:56 -0700

    ci: install GH cli in the ubuntu image
    This time for real; I edited the wrong script last time

--- assets/ci/emit-builder-dockerfile.py
+++ assets/ci/emit-builder-dockerfile.py
@@ -69,6 +69,12 @@ if "ubuntu" in container:
                 ]
             )
         ]
+        + [
+            "curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | gpg --dearmor -o /usr/share/keyrings/githubcli-archive-keyring.gpg",
+            'echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | tee /etc/apt/sources.list.d/github-cli.list > /dev/null',
+            "apt update",
+            "apt install -yqq --no-install-recommends gh",
+        ]
     )
 
     dockerfile += "ENV DEBIAN_FRONTEND=noninteractive\n"
