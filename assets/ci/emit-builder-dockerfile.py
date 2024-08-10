5b7813e0d7420ed4ffe357a908db56240fda1b50
Wez Furlong <wez@wezfurlong.org>
2024-08-09 21:09:40 -0700

    ci: remove gcc-9 from builder images
    aws-lc-sys refuses to build with it. aws-lc-sys is a dep of rustls.
    gcc-9 is normally present in ubuntu-20.04.
    The bug is not present in clang (what we end up with on ubuntu 20.04
    when we uninstall gcc-9) and gcc-10.

--- assets/ci/emit-builder-dockerfile.py
+++ assets/ci/emit-builder-dockerfile.py
@@ -71,6 +71,7 @@ if "ubuntu" in container:
             )
         ]
         + [
+            "apt remove gcc-9 || true", # it is broken and aws-lc-rs refuses to build with it
             "curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | gpg --dearmor -o /usr/share/keyrings/githubcli-archive-keyring.gpg",
             'echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | tee /etc/apt/sources.list.d/github-cli.list > /dev/null',
             "apt update",
