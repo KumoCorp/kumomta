370f5485990b54c93fed9da76d8e1e96cabb2072
Dirkjan Ochtman <dirkjan@ochtman.nl>
2024-09-25 15:17:58 +0200

    spf: move tests into a separate file

---
+++ crates/kumo-spf/src/tests.rs
@@ -0,0 +1,126 @@
+use crate::dns::Lookup;
+use crate::error::SpfError;
+use crate::{CheckHostParams, SpfDisposition, SpfResult};
+use futures::future::BoxFuture;
+use hickory_proto::rr::rdata::TXT;
+use hickory_proto::rr::{LowerName, RData, RecordData, RecordSet, RecordType, RrKey};
+use hickory_proto::serialize::txt::Parser;
+use hickory_resolver::Name;
+use std::collections::BTreeMap;
+use std::net::{IpAddr, Ipv4Addr};
+use std::str::FromStr;
+
+/// https://www.rfc-editor.org/rfc/rfc7208#appendix-A.1
+#[tokio::test]
+async fn all() {
+    let resolver = TestResolver::default()
+        .with_zone(EXAMPLE_COM)
+        .with_spf("example.com", "v=spf1 +all".to_string());
+
+    let result = CheckHostParams {
+        client_ip: IpAddr::V4(Ipv4Addr::LOCALHOST),
+        domain: "example.com".to_string(),
+        sender: "sender@example.com".to_string(),
+    }
+    .run(&resolver)
+    .await;
+
+    k9::assert_equal!(
+        &result,
+        &SpfResult {
+            disposition: SpfDisposition::Pass,
+            context: "matched 'all' directive".to_owned(),
+        },
+        "{result:?}"
+    );
+}
+
+/// https://www.rfc-editor.org/rfc/rfc7208#appendix-A
+const EXAMPLE_COM: &str = r#"; A domain with two mail servers, two hosts, and two servers
+; at the domain name
+$ORIGIN example.com.
+@       600 MX  10 mail-a
+            MX  20 mail-b
+            A   192.0.2.10
+            A   192.0.2.11
+amy         A   192.0.2.65
+bob         A   192.0.2.66
+mail-a      A   192.0.2.129
+mail-b      A   192.0.2.130
+www         CNAME example.com."#;
+
+#[derive(Default)]
+struct TestResolver {
+    records: BTreeMap<Name, BTreeMap<RrKey, RecordSet>>,
+}
+
+impl TestResolver {
+    fn with_zone(mut self, zone: &str) -> Self {
+        let (name, records) = Parser::new(zone, None, None).parse().unwrap();
+        self.records.insert(name, records);
+        self
+    }
+
+    fn with_spf(mut self, domain: &str, policy: String) -> Self {
+        let fqdn = format!("{}.", domain);
+        let authority = Name::from_str(&fqdn).unwrap();
+        let key = RrKey {
+            name: LowerName::from_str(&fqdn).unwrap(),
+            record_type: RecordType::TXT,
+        };
+
+        let mut records = RecordSet::new(&authority, RecordType::TXT, 0);
+        records.add_rdata(RData::TXT(TXT::new(vec![policy])));
+        self.records
+            .entry(authority)
+            .or_insert_with(BTreeMap::new)
+            .insert(key, records);
+
+        self
+    }
+}
+
+impl Lookup for TestResolver {
+    fn lookup_txt<'a>(&'a self, full: &'a str) -> BoxFuture<'a, Result<Vec<String>, SpfError>> {
+        Box::pin(async move {
+            let (mut name, mut authority) = ("", full);
+            loop {
+                let authority_name = Name::from_str(authority).unwrap();
+                let Some(records) = self.records.get(&authority_name) else {
+                    match authority.split_once('.') {
+                        Some(new) => {
+                            name = new.0;
+                            authority = new.1;
+                            continue;
+                        }
+                        None => {
+                            println!("authority not found: {full}");
+                            return Err(SpfError::DnsRecordNotFound(full.to_string()));
+                        }
+                    }
+                };
+
+                let fqdn = format!("{full}.");
+                let key = RrKey {
+                    name: LowerName::from_str(&fqdn).unwrap(),
+                    record_type: RecordType::TXT,
+                };
+
+                let Some(records) = records.get(&key) else {
+                    println!("key not found: {key:?}");
+                    return Err(SpfError::DnsRecordNotFound(name.to_string()));
+                };
+
+                let mut values = vec![];
+                for record in records.records_without_rrsigs() {
+                    let txt = TXT::try_borrow(record.data().unwrap()).unwrap();
+                    for slice in txt.iter() {
+                        values.push(String::from_utf8(slice.to_vec()).unwrap());
+                    }
+                }
+
+                return Ok(values);
+            }
+        })
+    }
+}
