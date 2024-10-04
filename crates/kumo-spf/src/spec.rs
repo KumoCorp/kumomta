189a041a1693a843cac99fcbfa293ea8c5d6e401
Dirkjan Ochtman <dirkjan@ochtman.nl>
2024-10-04 15:29:10 +0200

    spf: move macro expansion tests into spec module

--- crates/kumo-spf/src/spec.rs
+++ crates/kumo-spf/src/spec.rs
@@ -327,3 +327,84 @@ impl MacroName {
         }
     }
 }
+
+#[cfg(test)]
+mod test {
+    use std::net::IpAddr;
+
+    use super::*;
+    use crate::spec::MacroSpec;
+
+    #[test]
+    fn test_eval() {
+        // <https://datatracker.ietf.org/doc/html/rfc7208#section-7.4>
+
+        let mut ctx = SpfContext::new(
+            "strong-bad@email.example.com",
+            "email.example.com",
+            IpAddr::from([192, 0, 2, 3]),
+        )
+        .unwrap();
+
+        for (input, expect) in &[
+            ("%{s}", "strong-bad@email.example.com"),
+            ("%{o}", "email.example.com"),
+            ("%{d}", "email.example.com"),
+            ("%{d4}", "email.example.com"),
+            ("%{d3}", "email.example.com"),
+            ("%{d2}", "example.com"),
+            ("%{d1}", "com"),
+            ("%{dr}", "com.example.email"),
+            ("%{d2r}", "example.email"),
+            ("%{l}", "strong-bad"),
+            ("%{l-}", "strong.bad"),
+            ("%{lr}", "strong-bad"),
+            ("%{lr-}", "bad.strong"),
+            ("%{l1r-}", "strong"),
+        ] {
+            let spec = MacroSpec::parse(input).unwrap();
+            let output = spec.expand(&ctx).unwrap();
+            k9::assert_equal!(&output, expect, "{input}");
+        }
+
+        for (input, expect) in &[
+            (
+                "%{ir}.%{v}._spf.%{d2}",
+                "3.2.0.192.in-addr._spf.example.com",
+            ),
+            ("%{lr-}.lp._spf.%{d2}", "bad.strong.lp._spf.example.com"),
+            (
+                "%{lr-}.lp.%{ir}.%{v}._spf.%{d2}",
+                "bad.strong.lp.3.2.0.192.in-addr._spf.example.com",
+            ),
+            (
+                "%{ir}.%{v}.%{l1r-}.lp._spf.%{d2}",
+                "3.2.0.192.in-addr.strong.lp._spf.example.com",
+            ),
+            (
+                "%{d2}.trusted-domains.example.net",
+                "example.com.trusted-domains.example.net",
+            ),
+            ("%{c}", "192.0.2.3"),
+        ] {
+            let spec = MacroSpec::parse(input).unwrap();
+            let output = spec.expand(&ctx).unwrap();
+            k9::assert_equal!(&output, expect, "{input}");
+        }
+
+        ctx.client_ip = IpAddr::from([0x2001, 0xdb8, 0, 0, 0, 0, 0, 0xcb01]);
+        for (input, expect) in &[
+            (
+                "%{ir}.%{v}._spf.%{d2}",
+                "1.0.b.c.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0\
+                 .0.0.0.0.8.b.d.0.1.0.0.2.ip6._spf.example.com",
+            ),
+            ("%{c}", "2001:db8::cb01"),
+            ("%{C}", "2001%3adb8%3a%3acb01"),
+        ] {
+            let spec = MacroSpec::parse(input).unwrap();
+            let output = spec.expand(&ctx).unwrap();
+            k9::assert_equal!(&output, expect, "{input}");
+        }
+    }
+}
