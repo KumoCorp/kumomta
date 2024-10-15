09da95f586ef61cd3097b359a0d28a873811a52c
Wez Furlong <wez@wezfurlong.org>
2024-10-15 08:41:45 -0700

    ci: bump sccache size, simplify rocks/snappy logic
    we don't build shared at all, so we can remove the additional
    complexity of looking for .so files

--- assets/ci/populate-cache-vars.sh
+++ assets/ci/populate-cache-vars.sh
@@ -7,6 +7,7 @@ cat >.cache-env <<-EOT
 export RUSTC_WRAPPER=$HOME/.cargo/bin/sccache
 export SCCACHE_DIR=/build-cache/sccache
 export SCCACHE_DIRECT=true
+export SCCACHE_CACHE_SIZE="100G"
 export CARGO_INCREMENTAL=0
 export PATH="${PATH}"
 EOT
@@ -17,22 +18,12 @@ ROCKSDB_LIB_DIR=$(dirname /opt/kumomta/lib*/librocksdb.a)
 if test -d "${ROCKSDB_LIB_DIR}" ; then
   echo "export ROCKSDB_LIB_DIR=${ROCKSDB_LIB_DIR}" >> .cache-env
   echo "export ROCKSDB_STATIC=static" >> .cache-env
-else
-  ROCKSDB_LIB_DIR=$(dirname /opt/kumomta/lib*/librocksdb.so)
-  if test -d "${ROCKSDB_LIB_DIR}" ; then
-    echo "export ROCKSDB_LIB_DIR=${ROCKSDB_LIB_DIR}" >> .cache-env
-  fi
 fi
 
 SNAPPY_LIB_DIR=$(dirname /opt/kumomta/lib*/libsnappy.a)
 if test -d "${SNAPPY_LIB_DIR}" ; then
   echo "export SNAPPY_LIB_DIR=${SNAPPY_LIB_DIR}" >> .cache-env
   echo "export SNAPPY_STATIC=static" >> .cache-env
-else
-  SNAPPY_LIB_DIR=$(dirname /opt/kumomta/lib*/libsnappy.so)
-  if test -d "${SNAPPY_LIB_DIR}" ; then
-    echo "export SNAPPY_LIB_DIR=${SNAPPY_LIB_DIR}" >> .cache-env
-  fi
 fi
 
 cat .cache-env
