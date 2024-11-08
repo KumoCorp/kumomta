24b4e4867cfc6dcb6df47c691e62a1e7fce2a611
Dirkjan Ochtman <dirkjan@ochtman.nl>
2024-11-08 15:07:11 +0100

    Use new rustls PEM API

--- crates/kumo-server-common/src/tls_helpers.rs
+++ crates/kumo-server-common/src/tls_helpers.rs
@@ -1,5 +1,6 @@
 use anyhow::Context;
 use data_loader::KeySource;
+use rustls::pki_types::pem::PemObject;
 use rustls::pki_types::{CertificateDer, PrivateKeyDer, PrivatePkcs8KeyDer};
 use rustls::ServerConfig;
 use std::sync::Arc;
@@ -11,10 +12,8 @@ pub async fn make_server_config(
 ) -> anyhow::Result<Arc<ServerConfig>> {
     let mut certificates = vec![];
     let private_key = match tls_private_key {
-        Some(key) => {
-            let data = key.get().await?;
-            load_private_key(&data).with_context(|| format!("loading private key from {key:?}"))?
-        }
+        Some(key) => PrivateKeyDer::from_pem_slice(&key.get().await?)
+            .with_context(|| format!("loading private key from {key:?}"))?,
         None => {
             let key = rcgen::generate_simple_self_signed(vec![hostname.to_string()])?;
             certificates.push(CertificateDer::from_slice(key.cert.der()).into_owned());
@@ -24,7 +23,8 @@ pub async fn make_server_config(
 
     if let Some(cert_file) = tls_certificate {
         let data = cert_file.get().await?;
-        certificates = load_certs(&data)
+        certificates = CertificateDer::pem_slice_iter(&data)
+            .collect::<Result<Vec<_>, _>>()
             .with_context(|| format!("loading certificates from {cert_file:?}"))?;
     }
 
@@ -33,30 +33,4 @@ pub async fn make_server_config(
         .with_single_cert(certificates, private_key)?;
 
     Ok(Arc::new(config))
-}
-
-fn load_certs(data: &[u8]) -> anyhow::Result<Vec<CertificateDer<'static>>> {
-    let mut certs = vec![];
-    let mut reader = std::io::BufReader::new(data);
-    for res in rustls_pemfile::certs(&mut reader) {
-        let cert = res.context("failed to read PEM encoded certificates")?;
-        certs.push(cert);
-    }
-    Ok(certs)
-}
-
-fn load_private_key(data: &[u8]) -> anyhow::Result<PrivateKeyDer<'static>> {
-    let mut reader = std::io::BufReader::new(data);
-
-    loop {
-        match rustls_pemfile::read_one(&mut reader).expect("cannot parse private key .pem file") {
-            Some(rustls_pemfile::Item::Pkcs8Key(key)) => return Ok(key.into()),
-            Some(rustls_pemfile::Item::Pkcs1Key(key)) => return Ok(key.into()),
-            Some(rustls_pemfile::Item::Sec1Key(key)) => return Ok(key.into()),
-            None => break,
-            _ => {}
-        }
-    }
-
-    anyhow::bail!("no keys found in key data (encrypted keys not supported)",);
 }
