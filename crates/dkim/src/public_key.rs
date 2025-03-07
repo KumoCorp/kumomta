6cf315c37d4cadd81bee32fabc1e29769bbcf60c
Wez Furlong <wez@wezfurlong.org>
2025-03-07 09:05:02 -0700

    dkim: extract public key parsing guts
    No overall functional change, this is just moving the guts
    around a bit to support validating from multiple signatures
    in a later commit.

--- crates/dkim/src/public_key.rs
+++ crates/dkim/src/public_key.rs
@@ -7,6 +7,89 @@ use std::collections::HashMap;
 const RSA_KEY_TYPE: &str = "rsa";
 const ED25519_KEY_TYPE: &str = "ed25519";
 
+enum KeyType {
+    Rsa,
+    Ed25519,
+}
+
+impl KeyType {
+    fn parse(k: &str) -> Result<Self, DKIMError> {
+        match k {
+            RSA_KEY_TYPE => Ok(Self::Rsa),
+            ED25519_KEY_TYPE => Ok(Self::Ed25519),
+            _ => Err(DKIMError::InappropriateKeyAlgorithm),
+        }
+    }
+}
+
+fn parse_single_key(txt: &str) -> Result<DkimPublicKey, DKIMError> {
+    tracing::debug!("DKIM TXT: {:?}", txt);
+
+    // Parse the tags inside the DKIM TXT DNS record
+    let (_, tags) = parser::tag_list(txt).map_err(|err| {
+        tracing::warn!("key syntax error: {}", err);
+        DKIMError::KeySyntaxError
+    })?;
+
+    let mut tags_map = HashMap::new();
+    for tag in &tags {
+        tags_map.insert(tag.name.clone(), tag.clone());
+    }
+
+    // Check version
+    if let Some(version) = tags_map.get("v") {
+        if version.value != "DKIM1" {
+            return Err(DKIMError::KeyIncompatibleVersion);
+        }
+    }
+
+    // Get key type
+    let key_type = tags_map
+        .get("k")
+        .map(|v| v.value.as_str())
+        .unwrap_or(RSA_KEY_TYPE);
+    let key_type = KeyType::parse(key_type)?;
+
+    let tag = tags_map.get("p").ok_or(DKIMError::NoKeyForSignature)?;
+    let bytes = data_encoding::BASE64
+        .decode(tag.value.as_bytes())
+        .map_err(|err| {
+            DKIMError::KeyUnavailable(format!("failed to decode public key: {}", err))
+        })?;
+    match key_type {
+        KeyType::Rsa => Ok(DkimPublicKey::Rsa(
+            PKey::from_rsa(
+                Rsa::public_key_from_der(&bytes)
+                    .or_else(|_| Rsa::public_key_from_der_pkcs1(&bytes))
+                    .map_err(|err| {
+                        DKIMError::KeyUnavailable(format!("failed to parse public key: {}", err))
+                    })?,
+            )
+            .map_err(|err| {
+                DKIMError::KeyUnavailable(format!("failed to parse public key: {}", err))
+            })?,
+        )),
+        KeyType::Ed25519 => {
+            let mut key_bytes = [0u8; ed25519_dalek::PUBLIC_KEY_LENGTH];
+            if bytes.len() != key_bytes.len() {
+                return Err(DKIMError::KeyUnavailable(format!(
+                    "ed25519 public keys should be {} bytes in length, have: {}",
+                    ed25519_dalek::PUBLIC_KEY_LENGTH,
+                    bytes.len()
+                )));
+            }
+
+            key_bytes.copy_from_slice(&bytes);
+
+            Ok(DkimPublicKey::Ed25519(
+                ed25519_dalek::VerifyingKey::from_bytes(&key_bytes).map_err(|err| {
+                    DKIMError::KeyUnavailable(format!("failed to parse public key: {}", err))
+                })?,
+            ))
+        }
+    }
+}
+
 // https://datatracker.ietf.org/doc/html/rfc6376#section-6.1.2
 pub(crate) async fn retrieve_public_key(
     resolver: &dyn Resolver,
@@ -25,73 +108,7 @@ pub(crate) async fn retrieve_public_key(
     // rotation they are often multiple keys to consider.
     let txt = answer.as_txt();
     let first = txt.first().ok_or(DKIMError::NoKeyForSignature)?;
-    tracing::debug!("DKIM TXT: {:?}", first);
-
-    // Parse the tags inside the DKIM TXT DNS record
-    let (_, tags) = parser::tag_list(first).map_err(|err| {
-        tracing::warn!("key syntax error: {}", err);
-        DKIMError::KeySyntaxError
-    })?;
-
-    let mut tags_map = HashMap::new();
-    for tag in &tags {
-        tags_map.insert(tag.name.clone(), tag.clone());
-    }
-
-    // Check version
-    if let Some(version) = tags_map.get("v") {
-        if version.value != "DKIM1" {
-            return Err(DKIMError::KeyIncompatibleVersion);
-        }
-    }
-
-    // Get key type
-    let key_type = match tags_map.get("k") {
-        Some(v) => {
-            if v.value != RSA_KEY_TYPE && v.value != ED25519_KEY_TYPE {
-                return Err(DKIMError::InappropriateKeyAlgorithm);
-            }
-            v.value.clone()
-        }
-        None => RSA_KEY_TYPE.to_string(),
-    };
-
-    let tag = tags_map.get("p").ok_or(DKIMError::NoKeyForSignature)?;
-    let bytes = data_encoding::BASE64
-        .decode(tag.value.as_bytes())
-        .map_err(|err| {
-            DKIMError::KeyUnavailable(format!("failed to decode public key: {}", err))
-        })?;
-    let key = if key_type == RSA_KEY_TYPE {
-        DkimPublicKey::Rsa(
-            PKey::from_rsa(
-                Rsa::public_key_from_der(&bytes)
-                    .or_else(|_| Rsa::public_key_from_der_pkcs1(&bytes))
-                    .map_err(|err| {
-                        DKIMError::KeyUnavailable(format!("failed to parse public key: {}", err))
-                    })?,
-            )
-            .map_err(|err| {
-                DKIMError::KeyUnavailable(format!("failed to parse public key: {}", err))
-            })?,
-        )
-    } else {
-        let mut key_bytes = [0u8; ed25519_dalek::PUBLIC_KEY_LENGTH];
-        if bytes.len() != key_bytes.len() {
-            return Err(DKIMError::KeyUnavailable(format!(
-                "ed25519 public keys should be {} bytes in length, have: {}",
-                ed25519_dalek::PUBLIC_KEY_LENGTH,
-                bytes.len()
-            )));
-        }
-
-        key_bytes.copy_from_slice(&bytes);
-
-        DkimPublicKey::Ed25519(ed25519_dalek::VerifyingKey::from_bytes(&key_bytes).map_err(
-            |err| DKIMError::KeyUnavailable(format!("failed to parse public key: {}", err)),
-        )?)
-    };
-    Ok(key)
+    parse_single_key(first)
 }
 
 #[cfg(test)]
