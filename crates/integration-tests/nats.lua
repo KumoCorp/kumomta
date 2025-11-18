52e2eaae82b85a5850cf7e7c4126cc9d936bcbe3
Tobias Horst <mail@tobiashorst.de>
2025-11-18 13:50:00 +0100

    mod_nats: add integration tests

---
+++ crates/integration-tests/nats.lua
@@ -0,0 +1,68 @@
+local kumo = require 'kumo'
+package.path = '../../assets/?.lua;' .. package.path
+
+local TEST_DIR = os.getenv 'KUMOD_TEST_DIR'
+local ADDRESS = os.getenv 'ADDRESS'
+local SUBJECT = os.getenv 'SUBJECT'
+local USERNAME = os.getenv 'USERNAME'
+local PASSWORD = os.getenv 'PASSWORD'
+
+kumo.on('init', function()
+  kumo.configure_local_logs {
+    log_dir = TEST_DIR .. '/logs',
+    max_segment_duration = '1s',
+  }
+
+  kumo.define_spool {
+    name = 'data',
+    path = TEST_DIR .. '/data-spool',
+  }
+
+  kumo.define_spool {
+    name = 'meta',
+    path = TEST_DIR .. '/meta-spool',
+  }
+
+  local producer = kumo.nats.connect {
+    servers = { ADDRESS },
+    name = "nats-client",
+    auth = {
+      key_data = string.format(
+        [[
+          username = "%s"
+          password = "%s"
+        ]],
+        USERNAME,
+        PASSWORD
+      )
+    },
+  }
+
+  producer:publish {
+    subject = SUBJECT,
+    payload = "payload-1",
+    await_ack = true
+  }
+
+  producer:publish {
+    subject = SUBJECT,
+    payload = "payload-2",
+    await_ack = false
+  }
+
+  producer:publish {
+    subject = SUBJECT,
+    payload = "payload-3",
+    headers = {
+      ["Nats-Msg-Id"] = "unique-key-3",
+    },
+    await_ack = true
+  }
+
+  producer:close()
+
+  producer:publish {
+    subject = SUBJECT,
+    payload = "not sent because already closed",
+  }
+end)
