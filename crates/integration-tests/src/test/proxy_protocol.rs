ffe960e8eabad6a3816bc17bbe28798ad2aa29be
Wez Furlong <wez@wezfurlong.org>
2026-03-18 10:03:13 +0000

    tests: capture what happens if we can't connect to ha proxy
    This commit doesn't change any behavior, just captures the
    current behavior if an ha proxy server is not reachable.

--- crates/integration-tests/src/test/proxy_protocol.rs
+++ crates/integration-tests/src/test/proxy_protocol.rs
@@ -1,6 +1,7 @@
 use crate::kumod::{generate_message_text, DaemonWithMaildirOptions, MailGenParams};
 use anyhow::Context;
 use kumo_api_types::TraceSmtpV1Payload::Callback;
+use kumo_log_types::RecordType::TransientFailure;
 use std::time::Duration;
 
 #[tokio::test]
@@ -64,6 +65,68 @@ async fn proxy_protocol_switch_from_addr() -> anyhow::Result<()> {
     );
 
     daemon.stop_both().await.context("stop_both")?;
+
+    Ok(())
+}
+
+/// This test just captures what happens if the proxy is unreachable.
+/// The error message is likely linux-specific
+#[tokio::test]
+#[cfg(target_os = "linux")]
+async fn proxy_protocol_broken_proxy() -> anyhow::Result<()> {
+    let mut daemon = DaemonWithMaildirOptions::new()
+        .policy_file("broken-proxy-source.lua")
+        .start()
+        .await
+        .context("DaemonWithMaildir::start")?;
+
+    let mut client = daemon.smtp_client().await.context("make smtp_client")?;
+    let body = generate_message_text(1024, 78);
+    let response = MailGenParams {
+        body: Some(&body),
+        ..Default::default()
+    }
+    .send(&mut client)
+    .await?;
+    anyhow::ensure!(response.code == 250);
+
+    let sink_port = daemon.sink.listener("smtp").port();
+
+    daemon
+        .wait_for_source_summary(
+            |summary| summary.get(&TransientFailure).copied().unwrap_or(0) >= 1,
+            Duration::from_secs(5),
+        )
+        .await;
+
+    daemon.stop_both().await.context("stop_both")?;
+
+    let logs = daemon.source.collect_logs().await?;
+    let transient: Vec<String> = logs
+        .into_iter()
+        .filter_map(|r| match r.kind {
+            TransientFailure => Some(
+                format!("{:#?}", r.response)
+                    .replace(&format!(":{sink_port}"), ":PORT")
+                    .replace(&format!(": {sink_port}"), ": PORT"),
+            ),
+            _ => None,
+        })
+        .collect();
+    eprintln!("{transient:#?}");
+    k9::snapshot!(
+        &transient,
+        r#"
+[
+    "Response {
+    code: 400,
+    enhanced_code: None,
+    content: "KumoMTA internal: failed to connect to any candidate hosts: connect to 127.0.0.1:PORT and read initial banner: failed to connect to 255.255.255.255:1 HA { server: 255.255.255.255:1, addresses: IPv4(IPv4 { source_address: 127.0.0.1, source_port: 0, destination_address: 127.0.0.1, destination_port: PORT }), source: 127.0.0.1 }: Network is unreachable (os error 101)",
+    command: None,
+}",
+]
+"#
+    );
 
     Ok(())
 }
